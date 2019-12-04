e81eab108d5f9eaaeb21c4c9eca4d4ea705f3ab3
Matt Goodrich <mryangoodrich6@gmail.com>
2019-12-03 21:43:53 -0700

    D4 setup

---
+++ D4/first.js
@@ -0,0 +1,30 @@
+// ----------- PACKAGES -----------------------
+const fs = require('fs');
+// const {all, any, contains, enumerate, filter, iter, map, max, min, range, reduce, reduce_, sorted, sum, zip, zip3} = require('itertools');
+// const Map = require('collections/map');
+// const pycollections = require('pycollections');
+
+// ----------- INPUT SETUP --------------------
+const input = fs.readFileSync('inputs/input', 'utf8');
+
+// ----------- TESTING AND REGEX -----®---------
+const isTest = false;
+
+const nonWhitespace = /\S+/g;
+const positiveOrNegativeNumber = /(-?)(\d+)/g;
+const numberAndWord = /(-?)(\d+) \S+/g;
+const entireLine = /^.*$/gm
+
+const regex = entireLine;
+const matches = input.matchAll(new RegExp(regex));
+
+// ----------- SOLUTION ------------------------
+// match[0]: entire match
+// match[1..N]: group
+
+for (const match of matches) {
+  console.log(match[0]);
+}
+
+
+// ----------- GUESSES -------------------------
