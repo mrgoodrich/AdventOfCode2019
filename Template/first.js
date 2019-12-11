b30db9a1cbf193b971ba1ff5ccbd2909325a79d3
Matt Goodrich <mattgoodrich@mattgoodrich-macbookpro.roam.corp.google.com>
2019-12-10 21:53:49 -0700

    template

--- Template/first.js
+++ Template/first.js
@@ -1,6 +1,7 @@
 // ----------- PACKAGES -----------------------
 const fs = require('fs');
-const {all, any, contains, enumerate, filter, iter, map, max, min, partition, permutations, range, reduce, reduce_, sorted, sum, toArray, zip, zip3} = require('iter-tools');
+const {all, any, contains, enumerate, filter, iter, map, partition, permutations, range, reduce, reduce_, sorted, sum, toArray, zip, zip3} = require('iter-tools');
+const {max, min} = require('itertools');
 // const Map = require('collections/map');
 // const pycollections = require('pycollections');
 
